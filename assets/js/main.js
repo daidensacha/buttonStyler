893556b9cda1649b99108100147b774af5ac2061
daidensacha <daidensacha@gmail.com>
2021-04-05 09:27:34 +0200

    added rscolor colorpicker options to js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,20 @@ $(function() {
      
    // window load  
    };
+
+//    ============== jscolor picker options ====================================
+//Color picker options
+// These options apply to all color pickers on the page
+jscolor.presets.default = {
+	previewPosition:'right', 
+    previewSize:60, 
+    position:'top',
+    palette:'rgba(0,0,0,0) #fff #808080 #000 #996e36 #f55525 #ffe438 #88dd20 #22e0cd #269aff #bb1cd4',
+	backgroundColor:'rgba(61,20,130,1)', 
+	borderColor:'rgba(54,68,187,1)', 
+    width:139, 
+    height:80, 
+	controlBorderColor:'rgba(54,68,187,1)', 
+    sliderSize:12, 
+	shadowColor:'rgba(0,0,0,0.43)'
+};

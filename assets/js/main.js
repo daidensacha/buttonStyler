6d729b0dab255805095a58e013c00d1dfba7b555
daidensacha <daidensacha@gmail.com>
2021-04-05 09:03:57 +0200

    added the bootstrap navbar js

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+// NAVBAR OFFCANVAS JS
+$(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+   console.log("document is ready");
+     
+     
+       $('[data-toggle="offcanvas"], #navToggle').on('click', function () {
+       $('.offcanvas-collapse').toggleClass('open')
+     })
+     
+     
+   // document ready  
+   });
+   
+   
+   window.onload = function() {
+    // executes when complete page is fully loaded, including all frames, objects and images
+   console.log("window is loaded");
+     
+     
+   // window load  
+   };
